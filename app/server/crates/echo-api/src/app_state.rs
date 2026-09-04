81dd4b5019e84d6fd674a5607963686ee3911aa2
Tyrone Williams <298924897+tyronewilliamsjr@users.noreply.github.com>
2026-09-03 20:09:42 -0400

    Add secure cookie config option

--- app/server/crates/echo-api/src/app_state.rs
+++ app/server/crates/echo-api/src/app_state.rs
@@ -3,4 +3,5 @@ use sqlx::PgPool;
 #[derive(Clone)]
 pub struct AppState {
     pub pool: PgPool,
+    pub cookie_secure: bool,
 }
--- app/server/crates/echo-api/src/main.rs
+++ app/server/crates/echo-api/src/main.rs
@@ -13,7 +13,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db_url = "NO LEAKS";
 
     let pool = echo_db::connect(db_url).await?;
-    let app_state = AppState { pool };
+    let app_state = AppState {
+        pool,
+        cookie_secure: std::env::var("SECURE_COOKIES")
+            .map(|v| v == "true")
+            .unwrap_or(!cfg!(debug_assertions)),
+    };
 
     let application = app::create_app(app_state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
