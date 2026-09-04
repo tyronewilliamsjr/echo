943d31102e0b3f80bcdb1c37618e5b9a0b0bf17a
Tyrone Williams <298924897+tyronewilliamsjr@users.noreply.github.com>
2026-09-04 15:52:02 -0400

    Add tracing for logging

--- app/server/crates/echo-api/src/main.rs
+++ app/server/crates/echo-api/src/main.rs
@@ -1,3 +1,4 @@
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 mod app;
 mod app_state;
 mod auth;
@@ -8,10 +9,21 @@ mod users;
 use app_state::AppState;
 use error::ApiError;
 
+fn init_tracing() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "echo_api=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db_url = "NO LEAKS";
 
+    init_tracing();
     let pool = echo_db::connect(db_url).await?;
     let app_state = AppState {
         pool,
@@ -23,6 +35,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let application = app::create_app(app_state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
 
+    tracing::info!("API server starting");
     axum::serve(listener, application).await?;
 
     Ok(())
