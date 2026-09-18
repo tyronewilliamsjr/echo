349b14df6404963a1a179756ce9496efce6add20
Tyrone Williams <298924897+tyronewilliamsjr@users.noreply.github.com>
2026-09-17 22:23:28 -0400

    Add route for session data

--- app/server/crates/echo-api/src/auth/router.rs
+++ app/server/crates/echo-api/src/auth/router.rs
@@ -1,15 +1,16 @@
 use super::handlers;
 use crate::AppState;
 use crate::middleware::session::require_auth;
-use axum::{Router, middleware, routing::post};
+use axum::{Router, middleware, routing::get, routing::post};
 
 pub fn router(state: AppState) -> Router<AppState> {
     let unprotected_routes = Router::new()
         .route("/signup", post(handlers::email_signup))
-        .route("/login", post(handlers::password_login));
+        .route("/signin", post(handlers::password_login));
 
     let protected_routes = Router::new()
         .route("/logout", post(handlers::logout))
+        .route("/session", get(handlers::get_session_user))
         .layer(middleware::from_fn_with_state(state, require_auth));
 
     Router::new()
